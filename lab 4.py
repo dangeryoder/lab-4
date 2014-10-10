399b9986150fba57e68dad852c651354407940af
dangeryoder <dangeryoder@gmail.com>
2014-10-10 14:36:32 -0500

    lab 4

---
+++ lab 4.py
@@ -0,0 +1,17 @@
+# 70pt
+
+def turnIntoList(list1):
+    return list1
+    
+# 80pt
+
+def getFirstInList (list1):
+    return list1[0]
+    
+# 90pt
+def lastInList(list2):
+    length = len(list2)
+    lastPosition = length - 1
+    print list2[lastPosition]
+    
+    
